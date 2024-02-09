40cd744b6f7bedac9468ff398ae4da9eb54c6e52
andriamoroz <44933771+andriamoroz@users.noreply.github.com>
2024-02-09 16:13:26 +0200

    Update Add prefixes before the name.ps1

--- Add prefixes before the name.ps1
+++ Add prefixes before the name.ps1
@@ -1,8 +1,12 @@
+
 # Your prefix to put
 $prefix = "Your_prefix_"
-
 # Path to the file and File extension filter
 Get-ChildItem -Path "D:\new" -Filter "*.mp3" | ForEach-Object {
-  $newName = $prefix + $_.Name
-  Rename-Item -Path $_.FullName -NewName $newName
-} 
+  if (-not $_.Name.StartsWith($prefix)) {
+    $newName = $prefix + $_.Name
+    Rename-Item -Path $_.FullName -NewName $newName
+  } else {
+    Write-Host "The prefix has already been added to the file $($_.Name). Skip."
+  }
+}
